98c79cd9e39ad4ff30a65f947eda69b88b0d5038
Karlo Siladi <karlo.siladi@fer.hr>
2019-06-17 19:09:10 +0200

    BLE scanner runs in background thread

--- ble_sniffer.py
+++ ble_sniffer.py
@@ -1,43 +1,55 @@
-#!/usr/bin/env python
-
-from bluepy.btle import Scanner, DefaultDelegate
+from bluepy.btle import Scanner
+import threading
 import time
 import os
 
 import struct
 
-def main():
-    print("***BLE sniffer***")
-    scanner = Scanner()
-    startTime = time.time()
+class BleSnifferThread(object):
+    """
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
 
-    if os.path.exists("myData.txt"):
-        os.remove("myData.txt")
+    def __init__(self, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True  # Daemonize thread
+
+        thread.start()  # Start the execution
+
+    def run(self):
+        print("***BLE sniffer***")
+        scanner = Scanner()
+        startTime = time.time()
+
+        if os.path.exists("myData.txt"):
+            os.remove("myData.txt")
+        os.mknod("myData.txt")
+
+        while(True):
+            devices = scanner.scan(1.0)
+
+            for dev in devices:
+                if dev.addr == 'd7:d1:6a:27:c7:c8':
+                    for (adtype, desc, value) in dev.getScanData():
+                        value = value[6] + value[7] + value[4] + value[5] +value[2] +value[3]+ value[1]+ value[2]
+                        temp = (struct.unpack('!f', bytes.fromhex(value))[0])
+                        print("nRF52-DK Temperatura:  %f" % (temp))
 
 
-    while(True):
-        devices = scanner.scan(1.0)
+                if dev.addr == 'da:0b:5e:4f:af:97':
+                    for (adtype, desc, value) in dev.getScanData():
+                        value = value[6] + value[7] + value[4] + value[5] +value[2] +value[3]+ value[1]+ value[2]
+                        temp = (struct.unpack('!f', bytes.fromhex(value))[0])
+                        timestamp = time.time() - startTime
+                        print("Timestamp: %f " % timestamp)
+                        print("nRF52840-DK Temperatura:  %f" % (temp))
 
-        for dev in devices:
-            if dev.addr == 'd7:d1:6a:27:c7:c8':
-                for (adtype, desc, value) in dev.getScanData():
-                    value = value[6] + value[7] + value[4] + value[5] +value[2] +value[3]+ value[1]+ value[2]
-                    temp = (struct.unpack('!f', bytes.fromhex(value))[0])
-                    print("nRF52-DK Temperatura:  %f" % (temp))
-
-
-            if dev.addr == 'da:0b:5e:4f:af:97':
-                for (adtype, desc, value) in dev.getScanData():
-                    value = value[6] + value[7] + value[4] + value[5] +value[2] +value[3]+ value[1]+ value[2]
-                    temp = (struct.unpack('!f', bytes.fromhex(value))[0])
-                    timestamp = time.time() - startTime
-                    print("Timestamp: %f " % timestamp)
-                    print("nRF52840-DK Temperatura:  %f" % (temp))
-
-                    with open("myData.txt", 'a+') as myFile:
-                        myFile.write("%f,%f\n" %(timestamp, temp))
-
-
-if __name__ == "__main__":
-    main()
-
+                        with open("myData.txt", 'a+') as myFile:
+                            myFile.write("%f,%f\n" %(timestamp, temp))
