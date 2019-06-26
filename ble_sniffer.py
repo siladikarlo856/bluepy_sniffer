9ca1f1a85a46e3c83c97cf7eabbd31f06a02ec4a
Karlo Siladi <siladikarlo856@gmail.com>
2019-06-26 22:55:58 +0200

    magic numbers

--- ble_sniffer.py
+++ ble_sniffer.py
@@ -11,7 +11,9 @@ class BleSnifferThread(object):
     The run() method will be started and it will run in the background
     until the application exits.
     """
-    recordFile = "myData.txt"
+    recordFile = "myData.txt"  # txt file for measurement records
+    manufacturerSpecificData = 0xFF  # adv type: Manufacturer
+    packetLength = 5 * 2  # every byte 2 hex symbol
 
     def __init__(self, interval=1):
         """ Constructor
@@ -28,26 +30,32 @@ class BleSnifferThread(object):
     def appendMeasurement(self, timestamp, temp, boardId):
         # debug print
         print("Timestamp: %f \tDevice: %s -> Temperatura: %.2f" % (timestamp, boardId, temp))
+        # append record in file
         with open(self.recordFile, 'a+') as myFile:
             myFile.write("%f,%f,%s\n" % (timestamp, temp, boardId))
 
     def run(self):
+        # debug print
         print("***BLE sniffer***")
+
+        # get BLE Scanner object instance
         scanner = Scanner()
+        # get start time for timestamp
         startTime = time.time()
 
+        # clear previous measurements on start
         if os.path.exists(self.recordFile):
             os.remove(self.recordFile)
         os.mknod(self.recordFile)
 
         while True:
+            # scan for 500 ms
             devices = scanner.scan(0.5)
 
             for dev in devices:
                 for (adtype, desc, value) in dev.getScanData():
-
-                    # skip if adv type is not Manufacturer data or length is not 5 bytes
-                    if adtype != 0xFF or len(value) != 10:
+                    # check adv type and length
+                    if adtype != self.manufacturerSpecificData or len(value) != self.packetLength:
                         break
 
                     # string + little endian
@@ -59,10 +67,9 @@ class BleSnifferThread(object):
                     # get timestamp
                     timestamp = time.time() - startTime
 
+                    # first byte is device ID
                     if (value[0] + value[1]) == 'ab':
                         self.appendMeasurement(timestamp, temp, 'nRF52832')
 
                     if (value[0] + value[1]) == 'cd':
                         self.appendMeasurement(timestamp, temp, 'nRF52840')
-
-
